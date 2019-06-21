d186c59551e4007cd10e56d06d4ecc74ea927605
dewi setiani <dewisetiani780@gmail.com>
2019-06-21 14:25:58 +0700

    dewi

---
+++ app/Http/Controllers/Api/siswaController.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use App\user;
+use App\siswa;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class siswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $siswa = siswa::all();
+        if (!$siswa) {
+            $response = [
+                'success' => false,
+                'data' => 'Empty',
+                'message' => 'Siswa tidak ditemukan.'
+            ];
+            return response()->json($response, 404);
+        }
+
+        $response = [
+            'success' => true,
+            'data' => $siswa,
+            'message' => 'Berhasil.'
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // 1. tampung semua imputan ke $input;
+        $input = $request->all();
+
+        //2. Buat validasi ditampung ke $validator
+        $validator = Validator::make($input, [
+            'nama' => 'required|min::15'
+        ]);
+
+        // 3. Chek validasi
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'data' => 'Validator Error.',
+                'message' => $validator->error()
+            ];
+            return response()->json($response, 500);
+        }
+
+        // 4. buat fungsi untuk menghandle semua inputan ->
+        // dimasukan ke table
+        $siswa = Siswa::create($input);
+
+        // 5. menampilkan response
+        $response = [
+            'success' => true,
+            'data' => $siswa,
+            'message' => 'Siswa Berhasil ditambahkan.'
+        ];
+
+        // 6. tampilkan hasil
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $siswa = siswa::Find($id);
+        if (!$siswa) {
+            $response = [
+                'success' => false,
+                'data' => 'Empty',
+                'message' => 'Siswa tidak ditemukan.'
+            ];
+            return response()->json($response, 404);
+        }
+
+        $response = [
+            'success' => true,
+            'data' => $siswa,
+            'message' => 'Berhasil.'
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
