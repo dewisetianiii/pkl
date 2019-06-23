ae62985f1f2bffd7cc42f57624838a4f7aa404ef
dewisetiani <dewisetiani780@gmail.com>
2019-06-23 15:17:25 +0700

    dewi

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -31,7 +31,7 @@
                         <div class="top-header-content d-flex align-items-center justify-content-between">
                             <!-- Logo -->
                             <div class="logo">
-                                <a href="welcome"><img src="img/core-img/logo.png" alt=""></a>
+                                <a href="/"><img src="img/core-img/logo.png" alt=""></a>
                             </div>
 
                             <!-- Login Search Area -->
